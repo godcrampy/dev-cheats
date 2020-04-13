a042e84d74799ee777b329d76a38b0b6b4047666
Sahil <sahilbondre@gmail.com>
2020-04-13 19:05:49 +0530

    add cheat-sheet data and assets

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,9 +1,60 @@
 import * as React from "react";
+import { CardProps } from "../Card";
 
 import "./Body.scss";
 
 import git from "../../assets/images/git.png";
+import npm from "../../assets/images/npm.png";
+import docker from "../../assets/images/docker.png";
+import gatsby from "../../assets/images/gatsby.png";
+import markdown from "../../assets/images/markdown.png";
+import regex from "../../assets/images/regex.png";
+import vscode from "../../assets/images/vscode.png";
+import yarn from "../../assets/images/yarn.png";
 import Card from "../Card";
+
+const data: CardProps[] = [
+  {
+    title: "Git",
+    image: git,
+    link: "https://github.com/godcrampy/cheat-sheets/blob/master/git/git-cheatsheet.pdf",
+  },
+  {
+    title: "npm",
+    image: npm,
+    link: "https://github.com/godcrampy/cheat-sheets/blob/master/npm/npm-cheatsheet.pdf",
+  },
+  {
+    title: "Yarn",
+    image: yarn,
+    link: "https://github.com/godcrampy/cheat-sheets/blob/master/yarn/yarn-cheatsheet.pdf",
+  },
+  {
+    title: "Gatsby",
+    image: gatsby,
+    link: "https://github.com/godcrampy/cheat-sheets/blob/master/gatsby/gatsby-cheatsheet.pdf",
+  },
+  {
+    title: "Docker",
+    image: docker,
+    link: "https://github.com/godcrampy/cheat-sheets/blob/master/docker/docker-cheatsheet.pdf",
+  },
+  {
+    title: "VS Code",
+    image: vscode,
+    link: "https://github.com/godcrampy/cheat-sheets/blob/master/vscode/vscode-cheatsheet.pdf",
+  },
+  {
+    title: "Markdown",
+    image: markdown,
+    link: "https://github.com/godcrampy/cheat-sheets/blob/master/markdown/markdown-cheatsheet.pdf",
+  },
+  {
+    title: "Regex",
+    image: regex,
+    link: "https://github.com/godcrampy/cheat-sheets/blob/master/regex/regex-cheatsheet.pdf",
+  },
+];
 
 export interface BodyProps {}
 
@@ -14,9 +65,9 @@ class Body extends React.Component<BodyProps, BodyState> {
     return (
       <div className="Body">
         <div className="flex">
-          <Card title="Git" link="#" image={git} />
-          <Card title="Git" link="#" image={git} />
-          <Card title="Git" link="#" image={git} />
+          {data.map((e) => {
+            return <Card {...e} />;
+          })}
         </div>
       </div>
     );
